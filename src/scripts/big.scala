76ec517df55b3951fdfe9ada96194aa67c695ac3
Renato Cavalcanti <renato@xavena.eu>
2013-10-16 18:10:20 +0200

    added simple time measurement

--- src/scripts/big.scala
+++ src/scripts/big.scala
@@ -1,8 +1,10 @@
 import models.RepositoryBig
 import utils.Processor
 
-
-Processor.findBestQuotes(RepositoryBig.demand(100), RepositoryBig.offers(200)).map { result =>
+val start = System.currentTimeMillis()
+Processor.findBestQuotes(RepositoryBig.demand(20), RepositoryBig.offers(100)).map { result =>
   result.print(println)
 }
+val end = System.currentTimeMillis()
+println(s"total time: ${end - start}")
 
